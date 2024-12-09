4afe6ee4e47fb8f784b1293753e81ac5d85f5724
Єгор Трифоуцан <trifoutsannn@gmail.com>
2024-12-10 00:28:28 +0200

    якісь  зміни лол

--- master_project/view_model/FFTMethods.cs
+++ master_project/view_model/FFTMethods.cs
@@ -12,17 +12,13 @@ namespace master_project
     {
         public static (double periodInSeconds, double periodInPoints) FindPeriod(double[] signal, double samplingFrequency)
         {
-            // Перевірка вхідного сигналу
             if (signal == null || signal.Length == 0)
                 throw new ArgumentException("Сигнал не може бути порожнім");
 
             int N = signal.Length;
-
-            // Масиви для зберігання реальних та уявних частин
             double[] real = new double[N];
             double[] imag = new double[N];
 
-            // Обчислення дискретного перетворення Фур'є (DFT)
             for (int k = 0; k < N; k++)
             {
                 real[k] = 0;
@@ -35,30 +31,22 @@ namespace master_project
                 }
             }
 
-            // Обчислення спектру (модулів комплексних чисел)
-            double[] magnitudes = new double[N / 2]; // Аналізуємо лише першу половину спектру
+            double[] magnitudes = new double[N / 2];
             for (int i = 0; i < N / 2; i++)
             {
                 magnitudes[i] = Math.Sqrt(real[i] * real[i] + imag[i] * imag[i]);
             }
 
-            // Знаходження частоти з найвищою амплітудою
             int peakIndex = Array.IndexOf(magnitudes, magnitudes.Max());
 
             if (peakIndex == 0)
                 throw new Exception("Не вдалося знайти домінантну частоту");
 
-            // Домінантна частота в герцах
             double dominantFrequency = (double)peakIndex * samplingFrequency / N;
-
-            // Період у секундах
             double periodInSeconds = 1 / dominantFrequency;
-
-            // Період у кількості точок
             double periodInPoints = (double)N / peakIndex;
 
             return (periodInSeconds, periodInPoints);
-
         }
     }
 }
